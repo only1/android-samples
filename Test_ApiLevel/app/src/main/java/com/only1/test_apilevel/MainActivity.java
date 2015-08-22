0571b370aa34a28d248710e24579b7a70f1736ac
daehwang01 <daehwang01@sk.com>
2015-08-22 12:05:37 +0900

    Example Project (Wrong Practice)
    - I wanna tell you why you should take care of "API Level"
    - This Project doesn't have any errors. But this app cause 'cash' on API 20,21,22.
    - That is, This app doesn't guarantee compatibility on "Low-Level API Devices"
    - If you know problem and solution of this project,
      you would know exactly these Values (compileSdkVersion, targetSdkVersion, minSdkVersion) meanings
      and why support library is needed.

---
+++ Test_ApiLevel/app/src/main/java/com/only1/test_apilevel/MainActivity.java
@@ -0,0 +1,55 @@
+package com.only1.test_apilevel;
+
+import android.Manifest;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    private String[] REQUIRED_PERMISSIONS = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_PHONE_STATE};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                requestPermissions(REQUIRED_PERMISSIONS, 0);
+            }
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
